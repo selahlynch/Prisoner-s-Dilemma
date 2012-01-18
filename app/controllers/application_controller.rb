5394034d5cd17a6b99c03c314e6ca0bfb2033ecc
Selah Lynch <selahlynch@gmail.com>
2012-01-18 14:54:00 -0500

    display sentancing results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,19 +24,20 @@ class ApplicationController < ActionController::Base
     ##assuming two prisoners already in this game ##is this safe??
     @other_prisoner = nil
     @game.prisoners.each do |thispris| 
-      if thispris.id != prisoner_id
+#    puts "TESTING - thispris.id=" + thispris.id.class.to_s + ' prisoner_id=' + prisoner_id.class.to_s + " - DONE TESTING"
+      if thispris.id.to_i != prisoner_id.to_i  ##those to_i's are important
         @other_prisoner = thispris
       end
     end
     
-    
-    ##confused, this should be backwards??!! something is fucked up
-    @you_went = ! @prisoner.game_prisoners.where(:game_id == game_id).first.confess.nil?
-    @they_went = ! @other_prisoner.game_prisoners.where(:game_id == game_id).first.confess.nil?
+    @you_went = ! @prisoner.game_prisoners.find_by_game_id(game_id).confess.nil?
+    @they_went = ! @other_prisoner.game_prisoners.find_by_game_id(game_id).confess.nil?
 
-    puts "TESTING - " + @prisoner.id.to_s + ' ' + @other_prisoner.id.to_s + " - DONE TESTING"
-    puts "TESTING - " + @you_went.to_s + ' ' + @they_went.to_s + " - DONE TESTING"
-
+    if(@you_went && @they_went)
+      you_squealed = @prisoner.game_prisoners.find_by_game_id(game_id).confess
+      they_squealed = @other_prisoner.game_prisoners.find_by_game_id(game_id).confess
+      sentance(you_squealed, they_squealed)
+    end
 
   end
 
@@ -47,7 +48,6 @@ class ApplicationController < ActionController::Base
     game_id = params[:game_id]
 
     gp = GamePrisoner.where(:prisoner_id => prisoner_id, :game_id => game_id).first
-    #puts "TESTING - " + gp.to_s + " - DONE TESTING"
     gp.confess = TRUE
     gp.save!
 
@@ -67,6 +67,22 @@ class ApplicationController < ActionController::Base
    redirect_to(:action => 'play_game', :message => 'plead_innocent')
   end 
   
+  
+  def sentance(you_squeal, they_squeal)
+    if you_squeal && they_squeal
+      @your_sentance = 3
+      @their_sentance = 3
+    elsif !you_squeal && !they_squeal
+      @your_sentance = 1
+      @their_sentance = 1
+    elsif you_squeal && !they_squeal
+      @your_sentance = 0
+      @their_sentance = 12
+    elsif !you_squeal && they_squeal
+      @your_sentance = 12
+      @their_sentance = 0
+    end  
+  end
   
   
 end
