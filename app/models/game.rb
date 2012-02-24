238026c0954e1dbaf6f4706a0f4b5727fb89e02a
Selah Lynch <selahlynch@gmail.com>
2012-02-24 15:05:23 -0500

    cleaning up comments

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,15 +2,14 @@ class Game < ActiveRecord::Base
   has_many :game_prisoners
   has_many :prisoners, :through => :game_prisoners
   
- # accepts_nested_attributes_for :game_prisoners, :allow_destroy => true #, :reject_if => :new_record?
-
   validates :name, :length => {:minimum => 1, :maximum => 100}
 
   def prisoner_names
     prisoners.collect{|p| p.name}.join(', ')
   end
 
-  ##VIRTUAL ATTRIBUTE!!!
+  ## virtual attribute prisoner_ids - an array of integers
+  # corresponds to the ids of the prisoners who are playing this game
   def prisoner_ids
     @prisoner_ids || prisoners.collect{|p| p.id}
   end
@@ -18,12 +17,15 @@ class Game < ActiveRecord::Base
   def prisoner_ids=(id_array)
     @prisoner_ids = id_array.collect{|id| id.to_i};
   end
+
+##think about situations where someone updates using game.prisoners.create()
+
     
   after_save :assign_prisoners
 
-  
   private
-
+  # create or remove GamePrisoner records so that Game is 
+  # associated with the prisoners specified in @prisoner_ids
   def assign_prisoners
     if @prisoner_ids
 
@@ -41,10 +43,8 @@ class Game < ActiveRecord::Base
       ids_to_add.each do |prisoner_id|
         GamePrisoner.create(:game_id => game_id, :prisoner_id => prisoner_id)
       end
-
     end
   end
-
 
 end
 
