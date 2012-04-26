65550af726403860905bffbf9b2c7a1ee9c82ac0
Jamie Ly <jamie.ly@gmail.com>
2012-04-26 14:41:41 -0300

    Update public/js_practice/games.js

--- public/js_practice/games.js
+++ public/js_practice/games.js
@@ -1,99 +1,112 @@
+// @@@@ !!! we typically prefer camel-case when writing javascript
 
+// this global probably isn't needed
 var game_data;
 
+// scope everything
+// short syntax is preferred IIRC
+$(function() {
+  var tblGames = $("#table_games");
 
-$(document).ready(function(){
+  // do this stuff right away
+  $("#table_games tr").not(".header").remove();
 
-  $("#table_games").find("tr").not("tr.header").remove();
-
+  // no error handler set
   $.getJSON('http://localhost:3000/games/list.json', function(data) {
     game_data = data;
     for(i = 0; i < game_data.length; i++){
-      $("#table_games").append("<tr class='game_row' data-id='"+i+"'><td>" + game_data[i].name + "</td></tr>");
+      tblGames.append("<tr class='game_row' data-id='"+i+"'><td>" + game_data[i].name + "</td></tr>");
     }
   });
 
-  $("#table_games").find("tr.game_row").live("click", display_edit_form);
-
-  $("#table_games").find("button.ajax_submit").live("click", submit_form);
-
-});
-
-
-
-
-function display_edit_form(){
-
-  $("#table_games").find("tr.game_edit_row").remove();
-
-  var id = $(this).data("id");
+  tblGames.find("tr.game_row").live("click", display_edit_form);
+  tblGames.find("button.ajax_submit").live("click", submit_form);
   
-  $(this).after("<tr class='game_edit_row'><td ></td></tr>");
-  
-  $(this).next().find('td').append("<form class='edit_game'></form>");
-  
-  var form = $(this).next().find('form');
-  
-  form.append("<label for='name_input'>Name: </label><input type='text' name='game[name]' id='name_input_"+ id +"' value='"+ game_data[id].name +"'></input><br/>");
-  form.append("<select id='game_prisoners_"+ id +"' multiple='multiple' name='game[prisoner_ids][]' size=5></select><br/>");
-  form.append("<button class='ajax_submit' type='button' data-game_id='"+game_data[id].id+"'>Submit</button><br/>");
-  
-  var select = form.find('select');
-
-  //populate select box
-    $.getJSON('game_members.json', function(data){
-      for(var i = 0; i < data.length; i++){
-        //data[i].name
-        var select_string = "";
-        if (data[i].is_selected==="selected"){
-          select_string = "selected = 'selected'";
-        }
-        
-        select.append("<option value='"+data[i].id+"' "+ select_string +">"+data[i].name+"</option>");
-      }
-    });
-  
-  var x = "dum";
-}
-
-
-function submit_form(){
-  //$(this) is the button that I clicked
-
-  //serialize form input
-  var form_inputs = $(this).closest("form").serialize();
-  
-  var this_row = $(this).closest("tr.game_edit_row");
-  var prev_row = $(this).closest("tr.game_edit_row").prev();
-  
-  //ajax post to rails app
-  $.ajax({
-    type: 'POST',
-    url: "http://localhost:3000/games/update/"+$(this).data("game_id"),
-    data: form_inputs,
-    dataType: "html",
-    success: function(){
-      prev_row.find("td:nth-child(1)").html(this_row.find("input:text").val());          
-      this_row.remove(); 
-    },
-    error: function(jqXHR, textStatus, errorThrown){alert("failure!" + textStatus + errorThrown);}
+  // or use delegates
+  tblGame.on('click', function(evt){
+    var tgt = $(evt.target);
+    if(tgt.is('tr.game_row')) {
+      display_edit_form.apply(this);
+    }
+    else if(tgt.is('button.ajax_submit')) {
+      submit_form.apply(this);
+    }
+    // you might also want to evt.preventDefault() or evt.stopPropagation()
   });
 
-
-}
-
-
-
- 
-var x = function(){
-  var data = [3,4,6];
-  //put another function here
+  // add documentation
+  function display_edit_form(){
   
-}
-
-
-
-
-
-
-
+    tblGame.find("tr.game_edit_row").remove();
+  
+    var self = $(this),
+      id = self.data("id"),
+      td = self.after("<tr class='game_edit_row'><td ></td></tr>").children('td'),
+      form = $("<form class='edit_game'></form>").appendTo(td);
+    
+    // you should look into using a templating engine such as underscore, moustache or handlebars
+    form.append("<label for='name_input'>Name: </label><input type='text' name='game[name]' id='name_input_"+ id +"' value='"+ game_data[id].name +"'></input><br/>")
+      .append("<select id='game_prisoners_"+ id +"' multiple='multiple' name='game[prisoner_ids][]' size=5></select><br/>")
+      .append("<button class='ajax_submit' type='button' data-game_id='"+game_data[id].id+"'>Submit</button><br/>");
+    
+    var select = form.find('select#game_prisoners'+id);
+  
+    //populate select box
+    // no error handling
+    $.getJSON('game_members.json', function(data){
+      for(var i = 0; i < data.length; i++){
+        var option = $('<option></option>')
+          .innerHTML(data[i].name)
+          .attr('value', data[i].id)
+          .appendTo(select);
+        
+        if(data[i].is_selected==="selected") {
+          option.attr('selected','selected');
+        }
+      }
+    });
+  }
+  
+  
+  function submit_form(){
+    //$(this) is the button that I clicked
+  
+    var self = $(this);
+    //serialize form input
+    var form_inputs = self.closest("form").serialize();
+    
+    var this_row = self.closest("tr.game_edit_row");
+    var prev_row = this_row.prev();
+    
+    //ajax post to rails app
+    $.ajax({
+      type: 'POST',
+      url: "http://localhost:3000/games/update/" + self.data("game_id"),
+      data: form_inputs,
+      dataType: "html",
+      success: function(){
+        prev_row.find("td:first-child").html(this_row.find("input:text").val());          
+        this_row.remove(); 
+      },
+      // it's good to have an error handler!
+      error: function(jqXHR, textStatus, errorThrown){alert("failure!" + textStatus + errorThrown);}
+    });
+  
+  
+  }
+  
+  
+  
+   
+  var x = function(){
+    var data = [3,4,6];
+    //put another function here
+    
+  }
+});  
+  
+  
+  
+  
+  
+  
